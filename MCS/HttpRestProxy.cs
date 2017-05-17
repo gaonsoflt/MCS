82e612d0968bb50faaf8674ba3dcfe20ea1b18b3
가온소프트(주) <()>
2017-05-17 10:33:16 +0900

    add rest method post, put, delete and add check httpresponsestatuscode

--- MCS/HttpRestProxy.cs
+++ MCS/HttpRestProxy.cs
@@ -29,15 +29,76 @@ namespace MCS
 
         public async Task<T> GetAsync<T>(string requestUri) where T : class
         {
+            Console.WriteLine("[GET] HttpRequest: " + client.BaseAddress + requestUri);
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            //var streamTask = client.GetStreamAsync(requestUri);
+            //return serializer.ReadObject(await streamTask) as T;
+            HttpResponseMessage response = await client.GetAsync(requestUri);
+            Console.WriteLine ("[GET] HttpResponseMessage: " + response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                return serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as T;
+            }
+            return null;
+        }
+
+        public async Task<T> PostAsync<T>(string requestUri, HttpContent content) where T : class
+        {
+            Console.WriteLine("[POST] HttpRequest: " + client.BaseAddress + requestUri);
             var serializer = new DataContractJsonSerializer(typeof(T));
 
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var streamTask = client.GetStreamAsync(requestUri);
-            
-            return serializer.ReadObject(await streamTask) as T;
+            HttpResponseMessage response = await client.PostAsync(requestUri, content);
+            Console.WriteLine("[POST] HttpResponseMessage: " + response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                return serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as T;
+            }
+            return null;
+        }
+
+        public async Task<T> PutAsync<T>(string requestUri, HttpContent content) where T : class
+        {
+            Console.WriteLine("[PUT] HttpRequest: " + client.BaseAddress + requestUri);
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            HttpResponseMessage response = await client.PutAsync(requestUri, content);
+            Console.WriteLine("[PUT] HttpResponseMessage: " + response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                return serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as T;
+            }
+            return null;
+        }
+
+        public async Task<T> DeleteAsync<T>(string requestUri) where T : class
+        {
+            Console.WriteLine("[DELETE] HttpRequest: " + client.BaseAddress + requestUri);
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            HttpResponseMessage response = await client.DeleteAsync(requestUri);
+            Console.WriteLine("[DELETE] HttpResponseMessage: " + response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                return serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as T;
+            }
+            return null;
         }
     }
 }
